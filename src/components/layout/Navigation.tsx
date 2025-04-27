ed7fe994d312c19bf3aa5245895728aec01b1086
alohaloha <aloha_au_ia_oe_kai@yahoo.co.jp>
2025-04-27 19:40:44 +0900

    ホバー時の色を変更

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -1,52 +1,52 @@
 import Link from "next/link"
 import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
+    NavigationMenu,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
 } from "@/components/ui/navigation-menu"
-import { cn } from "@/lib/utils"
+import {cn} from "@/lib/utils"
 
 const routes = [
-  {
-    href: "/rules",
-    label: "ルール",
-  },
-  {
-    href: "/events",
-    label: "イベント",
-  },
-  {
-    href: "/gallery",
-    label: "ギャラリー",
-  },
-  {
-    href: "/blog",
-    label: "ブログ",
-  },
+    {
+        href: "/rules",
+        label: "ルール",
+    },
+    {
+        href: "/events",
+        label: "イベント",
+    },
+    {
+        href: "/gallery",
+        label: "ギャラリー",
+    },
+    {
+        href: "/blog",
+        label: "ブログ",
+    },
 ]
 
 interface MainNavProps {
-  className?: string
+    className?: string
 }
 
-export function MainNav({ className }: MainNavProps) {
-  return (
-    <NavigationMenu className={cn("flex items-center space-x-4 lg:space-x-6", className)}>
-      <NavigationMenuList>
-        {routes.map((route) => (
-          <NavigationMenuItem key={route.href}>
-            <NavigationMenuLink asChild>
-              <Link 
-                href={route.href}
-                className="text-sm font-medium transition-colors hover:text-primary"
-              >
-                {route.label}
-              </Link>
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-        ))}
-      </NavigationMenuList>
-    </NavigationMenu>
-  )
+export function MainNav({className}: MainNavProps) {
+    return (
+        <NavigationMenu className={cn("flex items-center space-x-4 lg:space-x-6", className)}>
+            <NavigationMenuList>
+                {routes.map((route) => (
+                    <NavigationMenuItem key={route.href}>
+                        <NavigationMenuLink asChild>
+                            <Link
+                                href={route.href}
+                                className="text-sm font-medium transition-colors hover:bg-cta"
+                            >
+                                {route.label}
+                            </Link>
+                        </NavigationMenuLink>
+                    </NavigationMenuItem>
+                ))}
+            </NavigationMenuList>
+        </NavigationMenu>
+    )
 } 
