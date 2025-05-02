740d7133088a11196b96d0da386986018640a1c0
alohaloha <aloha_au_ia_oe_kai@yahoo.co.jp>
2025-05-03 02:29:13 +0900

    delete debug code

--- src/app/(routes)/events/page.tsx
+++ src/app/(routes)/events/page.tsx
@@ -13,7 +13,6 @@ function EventDetail() {
     const router = useRouter();
     const searchParams = useSearchParams();
     const selectedEventId = searchParams.get('id');
-    console.log('selectedEventId:', selectedEventId);
     const [selectedEvent, setSelectedEvent] = useState<any>(null);
     const [isDialogOpen, setIsDialogOpen] = useState(!!selectedEventId);
 
