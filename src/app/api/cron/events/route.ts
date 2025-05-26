d7d5c0e3d528a9f2c80d2916d1d43c5063b69ee4
alohaloha <aloha_au_ia_oe_kai@yahoo.co.jp>
2025-05-26 11:42:05 +0900

    update: cron用にエンドポイントに認証を追加

--- src/app/api/cron/events/route.ts
+++ src/app/api/cron/events/route.ts
@@ -1,9 +1,22 @@
-import { NextResponse } from 'next/server';
+import {NextRequest, NextResponse} from 'next/server';
 import { run } from '@/scripts/fetchAndUpsertEvents';
 
-export async function GET() {
+export async function GET(request: NextRequest) {
     try {
         console.log('Starting event data sync...');
+        const authHeader = request.headers.get('authorization');
+        if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+            return new Response('Unauthorized', {
+                status: 401,
+            });
+        }
+
+        if (process.env.ENABLE_CRON !== 'true') {
+            return NextResponse.json({
+                message: `Skipped: current env is '${process.env.VERCEL_ENV}'`,
+            });
+        }
+
         await run();
         console.log('Event data sync completed successfully');
         
